636a7453c4397c095e210688d7d0f381c013048e
humwer <olegmazurok2011@yandex.ru>
2017-04-22 00:13:09 +0700

    Add files via upload

--- #1/Tree/Tree/Tree.cpp
+++ #1/Tree/Tree/Tree.cpp
@@ -7,61 +7,70 @@
 #include <windows.h>
 #include <conio.h>
 
-struct Node {
+struct inf { // Объявляем данные которыми обладает член генеалогического древа
 	char Num[10];
-	char Name[40];
-	int Year;
-	struct Node *l;
-	struct Node *r;
-};
-typedef struct Node *PNode;
-
-PNode Search(PNode Tree, char Num[]) {
-	if (!Tree) return 0;
-	Search(Tree->l, Num);
-	if (strcmp(Tree->Num, Num) == 0)
-	{
-		printf("Имя: ");
-		printf(Tree->Name);
-		printf("| Порядковый номер: ");
-		printf(Tree->Num);
-		return Tree;
-	}
-	Search(Tree->r, Num);
+	char Name[20];
+	char Surname[20];
+	char Year[12];
+	struct inf *l;
+	struct inf *r;
 };
 
-PNode CreateLeaf() {
-	char Num[10];
-	char Name[40];
-	int Year;
-	PNode NewNode = new Node;
-	printf("Введите номер: ");
-	scanf("%s", &Num);
-	printf("Введите имя: ");
-	scanf("%s", &Name);
-	printf("Введите дату рождения: ");
-	scanf("%d", &Year);
-	strcpy(NewNode->Num, Num);
-	strcpy(NewNode->Name, Name);
-	NewNode->Year = Year;
-	NewNode->l = NULL;
-	NewNode->r = NULL;
-	return NewNode;
-};
+typedef struct inf *ptrinf; // Создаём синоним структуры inf
 
-void ShowAllLeafs(PNode Tree) {
-	if (!Tree) return;
-	ShowAllLeafs(Tree->l);
-	printf("%s - %d\n", Tree->Name, Tree->Year);
-	ShowAllLeafs(Tree->r);
+class Tree {
+public:
+	ptrinf Search(ptrinf Tree, char Num[]) { // функция поиска необходимого корня
+		if (!Tree) return 0;
+		Search(Tree->l, Num);
+		if (strcmp(Tree->Num, Num) == 0)
+		{
+			printf("Имя: ");
+			printf(Tree->Name);
+			printf(" | Порядковый номер: ");
+			printf(Tree->Num);
+			return Tree;
+		}
+		Search(Tree->r, Num);
+	};
+
+	ptrinf add() { // функция добавления данных
+		char Num[10];
+		char Name[20];
+		char Surname[20];
+		char Year[12];
+		ptrinf inf2 = new inf;
+		printf("Введите номер: ");
+		scanf("%s", &Num);
+		printf("Введите имя: ");
+		scanf("%s", &Name);
+		printf("Введите фамилию: ");
+		scanf("%s", &Surname);
+		printf("Введите дату рождения: ");
+		scanf("%s", &Year);
+		strcpy(inf2->Num, Num);
+		strcpy(inf2->Year, Year);
+		strcpy(inf2->Name, Name);
+		strcpy(inf2->Surname, Surname);
+		inf2->r = NULL;
+		inf2->l = NULL;
+		return inf2;
+	};
+
+	void show(ptrinf Tree) { // функция вывода древа
+		if (!Tree) return;
+		show(Tree->l);
+		printf(" %s | Имя: %s %s - [Дата рождения: %s]\n", Tree->Num, Tree->Name, Tree->Surname, Tree->Year);
+		show(Tree->r);
+	};
 };
 
 void main() {
-	PNode Root, p1;
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	Tree x;
+	ptrinf Root, p1;
+	system("chcp 1251");
 	printf("    Создайте корень генеалогического дерева\n");
-	Root = CreateLeaf();
+	Root = x.add();
 	int input;
 	char Num[10];
 	while (1) {
@@ -75,21 +84,19 @@ void main() {
 			system("cls");
 			printf("Введите номер листа, к которому хотите обратиться: ");
 			scanf("%s", Num);
-			p1 = Search(Root, Num);
+			p1 = x.Search(Root, Num);
 			printf("\n    Добавление матери...\n");
-			p1->l = CreateLeaf();
+			p1->l = x.add();
 			printf("\n    Добавление отца...\n");
-			p1->r = CreateLeaf();
+			p1->r = x.add();
 			break;
 		case 2:
 			system("cls");
-			ShowAllLeafs(Root);
+			x.show(Root);
 			getch();
 			break;
 		case 3:
 			return;
-		default:
-			break;
 		}
 	}
 	return;
